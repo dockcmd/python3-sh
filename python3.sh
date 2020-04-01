f44240143a3befd20e42a4032fc915cc9ee200ed
Mona Lisa <srdrucker@gmail.com>
2020-03-31 21:23:12 -0500

    change home path mapping

--- python3.sh
+++ python3.sh
@@ -6,6 +6,6 @@ include github.com/dockcmd/shmod util.sh
 exec_or_dryrun \
   $(docker_run) \
   $(docker_user) \
-  $(docker_workdir) \
+  $(docker_home_workdir) \
   $(docker_image dcmd/python3 v3.6.7) \
   "$@"
